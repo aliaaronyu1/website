c96118f509e649de1bb1bd4f52c11e8b3832d90d
unknown <agyu@dons.usfca.edu>
2024-06-27 21:39:54 -0400

    added home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,21 @@
 import React, { Fragment } from "react";
-
+import "../css/home.css"
 
 const Home = () => {
 
 
     return (
         <Fragment>
-            <h1>Hello</h1>
+            <div>
+                <div>
+                    <h1>Ali Aaron Yu</h1>
+                    <h4 className="subheading">Software Engineer</h4>
+                    <p>Welcome to my website. You will find all my projects here.</p>
+                </div>
+                <div>
+                    <img src = "../img/Profilepic.jpeg" alt="picture of Ali Aaron Yu"/>
+                </div>
+            </div>
         </Fragment>
     )
 }
